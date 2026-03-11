f59ec0c9aa00f465caf042539d67575db7ee7d6a
shenzhe <shenzhe100@126.com>
2026-03-11 15:03:41 +0000

    添加测试用例

---
+++ GPUVersion/run/largeImage.cpp
@@ -0,0 +1,87 @@
+#include "vkWFR.hpp"
+#include <chrono>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
+
+int main() {
+  std::cout << "GPU Version - onlyWFR implementation" << std::endl;
+
+  int img_width, img_height, img_comp;
+  unsigned char *raw_image =
+      stbi_load("/home/shenzhe/WorkSpace/vkWFR/matlab/bigPicture/000001.bmp",
+                &img_width, &img_height, &img_comp, 1);
+
+  std::cout << "Load Image Width: " << img_width << " Height: " << img_height
+            << " Comp: " << img_comp << std::endl;
+  assert(raw_image != nullptr);
+  assert(img_comp == 3);
+
+  constexpr unsigned int roi_startX = 123, roi_startY = 198, roi_width = 422,
+                         roi_height = 166;
+  {
+
+    vkWFR wfrobj(img_width, img_height,
+                 {roi_startX, roi_startY, roi_width, roi_height},
+                 10,   // sigmax
+                 -0.5, // wxl
+                 0.1,  // wxi
+                 0.5,  // wxh
+                 10,   // sigmay
+                 -0.5, // wyl
+                 0.1,  // wyi
+                 0.5,  // wyh
+                 0.0); // thr (对于WFR不需要)
+
+    std::vector<double> result;
+
+    std::cout << "Please input any key to continue" << std::endl;
+    std::cin.get();
+    // 调用onlyWFR函数
+    auto start = std::chrono::steady_clock::now();
+    for (int i = 0; i < 100; i++)
+      result = wfrobj(std::vector<unsigned char>(
+          raw_image, raw_image + img_width * img_height));
+
+    auto end = std::chrono::steady_clock::now();
+    auto duration =
+        std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    std::cout << "Window Size 10x10 Time taken: " << duration.count() << " ms"
+              << std::endl;
+  }
+  {
+    vkWFR wfrobj(img_width, img_height,
+                 {roi_startX, roi_startY, roi_width, roi_height},
+                 20,   // sigmax
+                 -1,   // wxl
+                 0.1,  // wxi
+                 1,    // wxh
+                 20,   // sigmay
+                 -1,   // wyl
+                 0.1,  // wyi
+                 1,    // wyh
+                 0.0); // thr (对于WFR不需要)
+
+    std::vector<double> result;
+
+    std::cout << "Please input any key to continue" << std::endl;
+    std::cin.get();
+    // 调用onlyWFR函数
+    auto start = std::chrono::steady_clock::now();
+    for (int i = 0; i < 100; i++)
+      result = wfrobj(std::vector<unsigned char>(
+          raw_image, raw_image + img_width * img_height));
+
+    auto end = std::chrono::steady_clock::now();
+    auto duration =
+        std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    std::cout << "Window Size 20x20 Time taken: " << duration.count() << " ms"
+              << std::endl;
+  }
+
+  stbi_image_free(raw_image);
+
+  return 0;
+}
--- matlab/wft/handleImage.m
+++ matlab/wft/handleImage.m
@@ -1,4 +1,36 @@
 clc;
 clear;
+
+%% real image
 img=imread("../smallpicture/000750.bmp");
-res=wft2f("wfr",img,10,-0.5,0.1,0.5,10,-0.5,0.1,0.5,6);
+
+%rect=getrect();
+rect=[238 294 100 82];
+img_roi=imcrop(img,rect);
+imshow(img_roi);
+
+%% wft
+tic
+for ic=1:100
+res=wft2f('wfr',img_roi,10,-0.5,0.1,0.5,10,-0.5,0.1,0.5,ic);
+end
+toc
+figure;
+imagesc(res.r)
+% subplot(2,2,1);
+% imagesc(res.wx);
+% title("X Move");
+% subplot(2,2,2);
+% imagesc(res.wy);
+% title("Y Move");
+% subplot(2,2,3);
+% imagesc(res.phase);
+% title("Phase");
+% subplot(2,2,4);
+% imagesc(res.r);
+% title("R");
+
+mask=ones(size(res.r));
+[p Pathmap]=unwrapping_qg_trim(complex(res.r),mask);
+figure;
+imagesc(Pathmap);
